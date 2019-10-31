9cb5a34c5d4ee6a8dc35e6424d0e1cdbe9b662f6
Petri Luukkonen <petri.luukkonen@takamaki.fi>
2019-10-31 19:29:16 +0200

    Tehtävien 2.11 vastaukset.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 // //
@@ -43,22 +44,23 @@ const PersonForm = ({submissionHandler, handleNameChange, handlePhoneNumberChang
 
 const App = () => {
 
-  const [ persons, setPersons] = useState([
-    { id:1, name: 'Arto Hellas', number: '040-123456' },
-    { id:2, name: 'Ada Lovelace', number: '39-44-5323523' },
-    { id:3, name: 'Dan Abramov', number: '12-43-234345' },
-    { id:4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
-
+  const [ persons, setPersons ] = useState([]);
   const [ newName, setNewName ] = useState('');
   const [ newPhoneNumber, setNewPhoneNumber ] = useState('');
   const [ newSearchQueryText, setNewSearchQueryText ] = useState('');
+
+  const hook = () => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+  }
+  useEffect(hook, []);
 
   const filteredPersonList = (persons) => {
     if (!newSearchQueryText) return persons;
     return persons.filter(person => person.name.toLowerCase().includes(newSearchQueryText) || person.number.toLowerCase().includes(newSearchQueryText));
   }
-
   const addPerson = (event) => {
     event.preventDefault();
 
