a48a2db6059cf3598ab8e5e73e95b0eba5858378
Petri Luukkonen <petri.luukkonen@takamaki.fi>
2019-12-04 08:27:29 +0200

    Unit test for totalLikes method (vastaus tehtävään 4.4)

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sumLikes, item) => {
+    return sumLikes + item.likes;
+  }
+
+  return blogs.reduce(reducer, 0);
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
