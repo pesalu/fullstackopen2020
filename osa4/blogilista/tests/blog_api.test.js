0a59ad53452f0175fc6e019add578c2b3b3dac21
pesalu <psluukko@gmail.com>
2019-12-09 20:17:13 +0200

    Vastaukset tehtäviin 4.8, 4.9* ja 4.10.

---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const Blog = require('../models/blog');
+const supertest = require('supertest');
+const app = require('../app');
+
+const api = supertest(app);
+
+const initialBlogs = [
+  {
+    'title': 'Pekka ihmemaassa',
+    'author': 'Pekka Puurtimo',
+    'url': 'booky.com',
+    'likes': 1
+  },
+  {
+    'title': 'Antti Kaiross',
+    'author': 'Pekka Puurtimo Jr.',
+    'url': 'aksu.net',
+    'likes': 2
+  }
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+});
+
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+});
+
+test('all blogs are returned', async () => {
+  let response = await api.get('/api/blogs');
+  expect(response.body.length).toBe(initialBlogs.length);
+});
+
+test('the identifying field of blog is \'id\' ', async () => {
+  const response = await api.get('/api/blogs');
+  expect(response.body[0].id).toBeDefined();
+});
+
+test('a valid blog can be added ', async () => {
+  const testBlog = {
+    'title': 'Liisa Karjalassa',
+    'author': 'Pekka von Puurtimo',
+    'url': '',
+    'likes': 0
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(testBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const response = await api.get('/api/blogs');
+
+  const blogTitles = response.body.map(blog => blog.title)
+
+  expect(response.body.length).toBe(initialBlogs.length + 1)
+  expect(blogTitles).toContain(
+    'Liisa Karjalassa'
+  );
+});
+
+afterAll(() => {
+  mongoose.connection.close()
+});
