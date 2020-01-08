4e33d4b574a197dbef22ed7caadff2d9d0220a36
Petri Luukkonen <petri.luukkonen@takamaki.fi>
2020-01-08 20:45:32 +0200

    End point for creating and showing users. 4.15

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -4,6 +4,7 @@ const config = require('./utils/config');
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const blogRouter = require('./controllers/blogs');
+const userRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.static('build'));
 app.use(bodyParser.json());
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', userRouter);
 
 // Middlwares
 app.use(middleware.errorHandler);
