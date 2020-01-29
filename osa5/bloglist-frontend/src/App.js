0c8bdf9da05d41d82a71751be1721c95eea89b5c
Petri Luukkonen <petri.luukkonen@takamaki.fi>
2020-01-29 21:28:45 +0200

    Front end for login -form. Show blogs and name of logged user after successfull login. Tehtävä 5.1

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,40 +1,89 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react';
+import loginService from './services/login';
+import blogService from './services/blogs';
+import Blog from './components/Blog';
+import Notification from './components/Notification';
 
-// import logo from './logo.svg';
-// import './App.css';
-
-function App() {
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
-  const handleLogin = (event) => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('logging in with', username, password)
+    console.log('logging in with', username, password);
+    const credentials = {username, password};
+    try {
+      const user = await loginService.login(credentials);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+
+      await blogService
+        .getAll()
+        .then(initialBlogs => setBlogs(initialBlogs));
+    } catch (error) {
+      console.log('ERROR ', error.message);
+      setErrorMessage('Wrong credentials');
+      setTimeout(() => setErrorMessage(null), 5000);
+    }
+  }
+
+  const getBlogComponents = () => blogs.map(blog =>
+    <Blog
+      key={blog.id}
+      blog={blog}
+    />
+  )
+
+  const loginForm = () => {
+    if (user === null) {
+      return (
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+              <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+              <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      )
+    } else {
+      return (
+        <div>
+        <p>{user.name} logged in</p>
+        {getBlogComponents()}
+        {/* {noteForm()} */}
+        </div>
+      )
+    }
+
   }
 
   return (
-    <form onSubmit={handleLogin}>
-    <div>
-      username
-        <input
-        type="text"
-        value={username}
-        name="Username"
-        onChange={({ target }) => setUsername(target.value)}
-      />
-    </div>
-    <div>
-      password
-        <input
-        type="password"
-        value={password}
-        name="Password"
-        onChange={({ target }) => setPassword(target.value)}
-      />
-    </div>
-    <button type="submit">login</button>
-  </form>
+    <>
+      <h1>Blogit</h1>
+
+      <Notification message={errorMessage} />
+      { loginForm() }
+    </>
   );
 }
 
-export default App;
+export default App
