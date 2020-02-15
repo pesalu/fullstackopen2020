87401ad432b8857a815552b1250d5d5f6baa3a11
Petri Luukkonen <petri.luukkonen@takamaki.fi>
2020-02-15 20:20:45 +0200

    Possible to expand other info of a blog. Tehtävä 5.6.*

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,42 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+
+  const [expanded, setExpanded] = useState('');
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const expandBlog = () => setExpanded(!expanded);
+  const showWhenVisible = { display: expanded ? '' : 'none' };
+
+  console.log('BLOG ', blog);
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => expandBlog()}>
+        {blog.title}
+      </div>
+      {blog.author}
+      <div style={showWhenVisible}>
+        <div>
+          <a href={blog.url}>{blog.url}</a>
+        </div>
+        <div>
+          {blog.likes} likes <button>Like</button>
+        </div>
+        <div>
+          {
+            blog.user ? 'Added by user ' + blog.user.name : ''
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
