a457806a71f75fc016ea0b876451cbc44d1f7057
Petri Luukkonen <petri.luukkonen@takamaki.fi>
2020-02-19 19:48:23 +0200

    Sort blogs by likes in descending order. Tehtävä 5.8*

--- osa5/bloglist-frontend/src/components/Blogs.js
+++ osa5/bloglist-frontend/src/components/Blogs.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Blog from './Blog'
 
-const Blogs = ({blogs, handleLikesIncrements}) => (
-  blogs.map(blog =>
+const Blogs = ({blogs, handleLikesIncrements}) => {
+  blogs.sort((blogA, blogB) => blogB.likes - blogA.likes);
+  return (blogs.map(blog =>
     <Blog
       key={blog.id}
       blog={blog}
       handleLikesIncrements={handleLikesIncrements}
     />)
-);
+  );
+}
 
 export default Blogs;
