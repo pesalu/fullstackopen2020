c20c350db0e59a22d956ad5dcb2b91a59e62c50b
Petri Luukkonen <petri.luukkonen@takamaki.fi>
2020-07-08 18:42:48 +0300

    Refactored Blog test suite code to create blog before each individual test.

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -14,41 +14,51 @@ const blog = {
 const onClick = () => {
   console.log('Klikattu!');
 };
+describe('<Blog />', () => {
 
-test('renders title and author', () => {
-  const component = render(<Blog blog={blog}  handleLikesIncrements={onClick} handleRemovalOfBlog={onClick}/>);
-  // expect(component.container).toHaveTextContent('Koiran päivät Kalle Päätalo');
-  const titleDiv = component.container.querySelector('.title');
-  expect(titleDiv).toHaveTextContent('Koiran päivät');
+  let component;
+  beforeEach(() => {
+    component = render(<Blog blog={blog}  handleLikesIncrements={onClick} handleRemovalOfBlog={onClick}/>);
+  });
 
-  const authorDiv = component.container.querySelector('.author');
-  expect(authorDiv).toHaveTextContent('Kalle Päätalo');
+  test('renders title and author', () => {
+    // const component = render(<Blog blog={blog}  handleLikesIncrements={onClick} handleRemovalOfBlog={onClick}/>);
+    // expect(component.container).toHaveTextContent('Koiran päivät Kalle Päätalo');
+    const titleDiv = component.container.querySelector('.title');
+    expect(titleDiv).toHaveTextContent('Koiran päivät');
+  
+    const authorDiv = component.container.querySelector('.author');
+    expect(authorDiv).toHaveTextContent('Kalle Päätalo');
+  
+    const urlDiv = component.container.querySelector('.details');
+    expect(urlDiv).toHaveStyle('display: none');
+  
+  });
+  
+  test('clicking title -div shows details (url and likes) of the blog', () => {
+    // const component = render(<Blog blog={blog}  handleLikesIncrements={onClick} handleRemovalOfBlog={onClick}/>);
+  
+    const titleDiv = component.container.querySelector('.title');
+    fireEvent.click(titleDiv);
+  
+    const detailsDiv = component.container.querySelector('.details');
+    expect(detailsDiv).not.toHaveStyle('display: none');
+  });
+  
+  test('clicking like -button two times calls event handler twice', () => {
+    const mockHandler = jest.fn();
+    component = render(<Blog blog={blog}  handleLikesIncrements={mockHandler} handleRemovalOfBlog={onClick}/>);
 
-  const urlDiv = component.container.querySelector('.details');
-  expect(urlDiv).toHaveStyle('display: none');
+    const titleDiv = component.container.querySelector('.title');
+    fireEvent.click(titleDiv);
+   
+    const likeButton = component.container.querySelector('.likeButton');
 
+    fireEvent.click(likeButton);
+    fireEvent.click(likeButton);
+    expect(mockHandler.mock.calls).toHaveLength(2);
+  });
+  
 });
 
-test('clicking title -div shows details (url and likes) of the blog', () => {
-  const component = render(<Blog blog={blog}  handleLikesIncrements={onClick} handleRemovalOfBlog={onClick}/>);
 
-  const titleDiv = component.container.querySelector('.title');
-  fireEvent.click(titleDiv);
-
-  const detailsDiv = component.container.querySelector('.details');
-  expect(detailsDiv).not.toHaveStyle('display: none');
-});
-
-test('clicking like -button two times calls event handler twice', () => {
-  const mockHandler = jest.fn();
-  const component = render(<Blog blog={blog}  handleLikesIncrements={mockHandler} handleRemovalOfBlog={onClick}/>);
-
-  const titleDiv = component.container.querySelector('.title');
-  fireEvent.click(titleDiv);
-
-  const likeButton = component.container.querySelector('.likeButton');
-
-  fireEvent.click(likeButton);
-  fireEvent.click(likeButton);
-  expect(mockHandler.mock.calls).toHaveLength(2);
-});
