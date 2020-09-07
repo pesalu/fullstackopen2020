47266d4b8558adeb1399c5d5c44098e3c741a7bd
Petri Luukkonen <psluukko@gmail.com>
2020-09-07 10:24:41 +0300

    Add project redux-anecdotes.

---
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -0,0 +1,59 @@
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
+const initialState = anecdotesAtStart.map(asObject)
+
+const reducer = (state = initialState, action) => {
+  console.log('state now: ', state)
+  console.log('action', action)
+
+  switch(action.type) {
+    case 'VOTE':
+      let state2 = state.map(anec => {
+        if (anec.id === action.data.id) {
+          anec.votes += 1;
+        };
+        console.log('ANEC ', anec)
+        return anec;
+      })
+      console.log('VOTE!! ', state2);
+      return state2;
+    case 'CREATE':
+      return [...state, action.data];
+    default: return state;
+  }
+}
+
+export const createNote = (anecdoteText) => {
+  return {
+    type: 'CREATE',
+    data: asObject(anecdoteText)
+  }
+}
+
+export const vote = (id) => {
+  console.log('vote', id)
+
+  return {
+    type: 'VOTE',
+    data: { id }
+  }
+};
+
+export default reducer
