56ba05d362c653e9fa8510c275d0e99e1e38d080
Petri Luukkonen <petri.luukkonen@takamaki.fi>
2020-11-17 19:28:42 +0200

    Answer to 8.8. Fetch authors from back-end.

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -3,9 +3,27 @@ import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+query {
+  allAuthors {
+    name 
+    born
+    bookCount
+  }
+}
+`
+
 
 const App = () => {
   const [page, setPage] = useState('authors')
+
+  const result = useQuery(ALL_AUTHORS)
+
+  if(result.loading) {
+    return <div>loading... </div>
+  }
 
   return (
     <div>
@@ -17,6 +35,7 @@ const App = () => {
 
       <Authors
         show={page === 'authors'}
+        authors={result.data.allAuthors}
       />
 
       <Books
