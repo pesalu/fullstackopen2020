00b5ba9223428c806eeff5b1d0118945dcb5f3b3
Petri Luukkonen <petri.luukkonen@takamaki.fi>
2020-12-07 19:18:45 +0200

    Answer to 8.19. Possible to filter books by their genres.

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries';
 
 const Books = (props) => {
+
+  let [genre, setGenre] = useState(null);
 
   const result = useQuery(ALL_BOOKS)
 
@@ -16,6 +18,17 @@ const Books = (props) => {
 
   const books = result.data.allBooks;
 
+  const filteredBooks = genre && genre !== 'all genres' ? books.filter(book => book.genres.includes(genre)) : books;
+
+  const genres = new Set(); 
+  books.forEach(book => {
+    book.genres.forEach(
+      genre => genres.add(genre)
+    )
+  });
+  const genresA = [...genres];
+  
+  console.log(genres)
   return (
     <div>
       <h2>books</h2>
@@ -33,7 +46,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
+          {filteredBooks.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author != null ? a.author.name : null}</td>
@@ -42,6 +55,12 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+
+      <div>
+          {genresA.map(genre => 
+            <button key={genre} onClick={() => setGenre(genre)}>{genre}</button>)}
+          <button onClick={() => setGenre('all genres')}>All genres</button>
+      </div>
     </div>
   )
 }
